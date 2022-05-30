1844bbd519039bca80411e018ea2185f48a29c04
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2022-05-30 15:02:30 +0100

    update about me text

--- components/sections/AboutMe.js
+++ components/sections/AboutMe.js
@@ -11,14 +11,14 @@ function AboutMe() {
       <div className='md:flex md:items-center md:gap-16'>
         <div className='md:w-1/2'>
           <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
-            I have been, for the past 5 years, helping clients establish their online
-            presence and use Social Media Marketing and Search Engine Marketing
-            effectively to generate conversions.
+            I have been, for the past 5 years, helping clients use Social Media Marketing
+            as well as Search Engine Marketing to move customers throughout the different
+            stages of their funnels leading ultimately to conversions.
           </p>
           <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
-            With 4+ years of experience in WordPress development under my belt, I
-            broadened my skillset to encompass Javascript, React, Node.js and Express.js
-            as an answer to my clients’ growing needs.
+            During that time, I’ve also accumulated 4+ years of experience in WordPress
+            development and broadened my skillset to encompass Javascript, React, Node.js
+            and Express.js as an answer to my clients’ growing needs.
           </p>
           <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
             I love what I do and I go above and beyond my due diligence for each client,
