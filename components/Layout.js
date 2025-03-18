cc33b25d7c07871cf2433f546ae36fbc38867926
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2025-03-17 21:04:16 -0400

    content update

--- components/Layout.js
+++ components/Layout.js
@@ -5,53 +5,73 @@ import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
 
 export default function Layout({ children, ...customMeta }) {
-  const { resolvedTheme } = useTheme();
-  const router = useRouter();
-  const meta = {
-    title: "Othmane Lamnabhi | MERN Developer",
-    description:
-      "I'm a Full Stack Developer and I love bringing ideas to live through code. (WordPress, React, Express.js, Node.js, MongoDB, Next.js)",
-    image: `https://othmanelamnabhi.com/images/og.jpg`,
-    ...customMeta,
-  };
+	const { resolvedTheme } = useTheme();
+	const router = useRouter();
+	const meta = {
+		title: "Othmane Lamnabhi | Frontend Developer",
+		description:
+			"I'm a Frontend Developer and I love bringing ideas to live through code. (React, Tanstack, Tailwind CSS, TypeScript, Node.js)",
+		image: `https://othmanelamnabhi.com/images/og.jpg`,
+		...customMeta,
+	};
 
-  return (
-    <div>
-      <Head>
-        <title>{meta.title}</title>
-        <meta name='description' content={meta.description} />
-        <meta property='og:type' content='website' />
-        <meta name='og:title' property='og:title' content={meta.title} />
-        <meta
-          name='og:description'
-          property='og:description'
-          content={meta.description}
-        />
-        <meta property='og:image:type' content='image/jpeg' />
-        <meta property='og:url' content={`https://othmanelamnabhi.com${router.asPath}`} />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content={meta.title} />
-        <meta name='twitter:description' content={meta.description} />
-        <meta name='twitter:site' content='@thewannabedev' />
-        <meta name='twitter:creator' content='@thewannabedev' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-        <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
-        <meta
-          name='theme-color'
-          content={resolvedTheme === "light" ? "#FFFFFE" : "#16161A"}
-        />
-        <meta property='og:image' content={meta.image} />
-        <meta name='twitter:image' content={meta.image} />
-        <link rel='canonical' href={`https://othmanelamnabhi.com${router.asPath}`} />
-      </Head>
-      <main className={`my-8`}>
-        <Header />
-        <div>{children}</div>
-        <Footer />
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>{meta.title}</title>
+				<meta name='description' content={meta.description} />
+				<meta property='og:type' content='website' />
+				<meta name='og:title' property='og:title' content={meta.title} />
+				<meta
+					name='og:description'
+					property='og:description'
+					content={meta.description}
+				/>
+				<meta property='og:image:type' content='image/jpeg' />
+				<meta
+					property='og:url'
+					content={`https://othmanelamnabhi.com${router.asPath}`}
+				/>
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:title' content={meta.title} />
+				<meta name='twitter:description' content={meta.description} />
+				<meta name='twitter:site' content='@thewannabedev' />
+				<meta name='twitter:creator' content='@thewannabedev' />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+				<meta
+					name='theme-color'
+					content={resolvedTheme === "light" ? "#FFFFFE" : "#16161A"}
+				/>
+				<meta property='og:image' content={meta.image} />
+				<meta name='twitter:image' content={meta.image} />
+				<link
+					rel='canonical'
+					href={`https://othmanelamnabhi.com${router.asPath}`}
+				/>
+			</Head>
+			<main className={`my-8`}>
+				<Header />
+				<div>{children}</div>
+				<Footer />
+			</main>
+		</div>
+	);
 }
--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -1,33 +1,33 @@
 import Image from "next/image";
 
 function Hero() {
-  return (
-    <section className='mt-12 px-8 sm:px-14 sm:text-center md:flex'>
-      <div className='md:w-1/2 md:pt-24 md:text-left'>
-        <h1 className='mb-3 font-Montserrat text-4xl font-bold text-custom-light-tarawera dark:text-custom-dark-off-by-one sm:text-5xl'>
-          Hi there,
-          <br />
-          I&apos;m Othmane.
-        </h1>
-        <p className='mb-4 font-Montserrat font-normal text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray sm:px-20 md:pl-0'>
-          I&apos;m a <span className='font-bold'>Full Stack Developer</span> and I love
-          bringing ideas to life through code.
-        </p>
-      </div>
-      <div className='md:w-1/2'>
-        <Image
-          src='/images/othmane_lamnabhi_headshot_hq_v2.png'
-          alt='Picture of Othmane Lamnabhi'
-          width={2413}
-          height={2480}
-          layout='responsive'
-          priority={true}
-          placeholder='empty'
-          sizes='50vw'
-        />
-      </div>
-    </section>
-  );
+	return (
+		<section className='mt-12 px-8 sm:px-14 sm:text-center md:flex'>
+			<div className='md:w-1/2 md:pt-24 md:text-left'>
+				<h1 className='mb-3 font-Montserrat text-4xl font-bold text-custom-light-tarawera dark:text-custom-dark-off-by-one sm:text-5xl'>
+					Hi there,
+					<br />
+					I&apos;m Othmane.
+				</h1>
+				<p className='mb-4 font-Montserrat font-normal text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray sm:px-20 md:pl-0'>
+					I&apos;m a <span className='font-bold'>Frontend Developer</span> and I
+					love bringing ideas to life through code.
+				</p>
+			</div>
+			<div className='md:w-1/2'>
+				<Image
+					src='/images/othmane_lamnabhi_headshot_hq_v2.png'
+					alt='Picture of Othmane Lamnabhi'
+					width={2413}
+					height={2480}
+					layout='responsive'
+					priority={true}
+					placeholder='empty'
+					sizes='50vw'
+				/>
+			</div>
+		</section>
+	);
 }
 
 export default Hero;
