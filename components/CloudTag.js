496d014706be7da4e1fba2d4e11f27c2d82dee15
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2025-03-19 17:59:29 -0400

    update content

--- components/CloudTag.js
+++ components/CloudTag.js
@@ -3,68 +3,74 @@ import { useTheme } from "next-themes";
 import useWindowSize from "./hooks/useWindowSize";
 
 const skills = [
-  "JavaScript",
-  "HTML",
-  "CSS",
-  "REST API",
-  "React",
-  "Redis",
-  "MUI",
-  "Node.js",
-  "Express.js",
-  "MongoDB",
-  "Next.js",
-  "Tailwind CSS",
-  "WordPress",
+	"REST API",
+	"React",
+	"Redis",
+	"MUI",
+	"Shadcn UI",
+	"Node.js",
+	"Express.js",
+	"MongoDB",
+	"MySQL",
+	"Next.js",
+	"Tailwind CSS",
+	"WordPress",
+	"Typescript",
+	"Tanstack",
 ];
 
 function CloudTag() {
-  const { resolvedTheme } = useTheme();
-  const { width } = useWindowSize();
+	const { resolvedTheme } = useTheme();
+	const { width } = useWindowSize();
 
-  useEffect(() => {
-    const options = {
-      textColour: resolvedTheme === "light" ? "#3DA9FC" : "#7F5AF0",
-      reverse: true,
-      depth: 0.8,
-      maxSpeed: 0.05,
-      initial: [0.1, -0.1],
-      activeCursor: "pointer",
-      wheelZoom: false,
-      decel: 1,
-      textHeight: 60,
-      outlineMethod: "none",
-      noSelect: true,
-      zoom: 0.9,
-      noMouse: width >= 768 ? false : true,
-    };
+	useEffect(() => {
+		const options = {
+			textColour: resolvedTheme === "light" ? "#3DA9FC" : "#7F5AF0",
+			reverse: true,
+			depth: 0.8,
+			maxSpeed: 0.05,
+			initial: [0.1, -0.1],
+			activeCursor: "pointer",
+			wheelZoom: false,
+			decel: 1,
+			textHeight: 60,
+			outlineMethod: "none",
+			noSelect: true,
+			zoom: 0.9,
+			noMouse: width >= 768 ? false : true,
+		};
 
-    try {
-      TagCanvas.Start("myCanvas", null, options);
-    } catch (e) {
-      // something went wrong, hide the canvas container
-      document.getElementById("myCanvasContainer").style.display = "none";
-    }
-  }, [resolvedTheme, width]);
-  return (
-    <>
-      <div id='myCanvasContainer' className='md:w-1/2'>
-        <canvas width='700' height='700' id='myCanvas' className='w-full font-bold'>
-          <p>
-            Anything in here will be replaced on browsers that support the canvas element
-          </p>
+		try {
+			TagCanvas.Start("myCanvas", null, options);
+		} catch (e) {
+			// something went wrong, hide the canvas container
+			document.getElementById("myCanvasContainer").style.display = "none";
+		}
+	}, [resolvedTheme, width]);
+	return (
+		<>
+			<div id='myCanvasContainer' className='md:w-1/2'>
+				<canvas
+					width='700'
+					height='700'
+					id='myCanvas'
+					className='w-full font-bold'>
+					<p>
+						Anything in here will be replaced on browsers that support the
+						canvas element
+					</p>
 
-          {skills.map((skill) => {
-            return (
-              <a href='#' key={skill}>
-                {skill}
-              </a>
-            );
-          })}
-        </canvas>
-      </div>
-    </>
-  );
+					{skills.map((skill) => {
+						return (
+							<a href='#' key={skill}>
+								{skill}
+							</a>
+						);
+					})}
+				</canvas>
+			</div>
+		</>
+	);
 }
 
 export default CloudTag;
--- components/sections/AboutMe.js
+++ components/sections/AboutMe.js
@@ -1,37 +1,44 @@
 import CloudTag from "../CloudTag";
 
 function AboutMe() {
-  return (
-    <section
-      id='about'
-      className='bg-custom-light-pattens-blue px-8 py-8 dark:bg-custom-dark-motorcycle-black sm:px-14 '>
-      <h2 className='mb-4 text-3xl font-bold text-custom-light-tarawera dark:text-custom-dark-off-by-one sm:text-4xl '>
-        About me
-      </h2>
-      <div className='md:flex md:items-center md:gap-16'>
-        <div className='md:w-1/2'>
-          <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
-            I have been, for the past 5 years, helping clients use Social Media Marketing
-            as well as Search Engine Marketing to move customers throughout the different
-            stages of their funnels leading ultimately to conversions.
-          </p>
-          <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
-            During that time, I’ve also accumulated 4+ years of experience in WordPress
-            development and broadened my skillset to encompass Javascript, React, Node.js
-            and Express.js as an answer to my clients’ growing needs.
-          </p>
-          <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
-            I love what I do and I go above and beyond my due diligence for each client,
-            bridging the gap between marketing and technology.
-          </p>
-          <p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
-            Here are a few technologies I’ve been working with recently:
-          </p>
-        </div>
-        <CloudTag />
-      </div>
-    </section>
-  );
+	return (
+		<section
+			id='about'
+			className='bg-custom-light-pattens-blue px-8 py-8 dark:bg-custom-dark-motorcycle-black sm:px-14 '>
+			<h2 className='mb-4 text-3xl font-bold text-custom-light-tarawera dark:text-custom-dark-off-by-one sm:text-4xl '>
+				About me
+			</h2>
+			<div className='md:flex md:items-center md:gap-16'>
+				<div className='md:w-1/2'>
+					<p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
+						I am a trilingual front-end developer with a strong foundation in
+						Tanstack, Typescript, React, Node.js and over five years of
+						experience building high-quality digital solutions. My journey
+						from corporate finance to tech has shaped my approach to
+						development—combining analytical thinking, problem-solving, and
+						project management to create solutions that are both functional
+						and impactful.
+					</p>
+					<p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
+						I specialize in developing scalable, accessible, and user-friendly
+						applications, bridging the gap between business objectives and
+						technical execution. Whether it’s optimizing a website’s
+						performance, enhancing user experience, or integrating complex
+						front-end components, I thrive on tackling challenges with a
+						holistic and strategic mindset.
+					</p>
+					<p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
+						I’m always learning and adapting, refining my skillset to stay
+						ahead of the curve.
+					</p>
+					<p className='mb-3 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray'>
+						Here are a few technologies I’ve been working with recently:
+					</p>
+				</div>
+				<CloudTag />
+			</div>
+		</section>
+	);
 }
 
 export default AboutMe;
