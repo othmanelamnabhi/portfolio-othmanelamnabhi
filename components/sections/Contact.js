9f5d72ec043f017860b3c65a80bdeadca747f49a
Othmane Lamnabhi <23281797+othmanelamnabhi@users.noreply.github.com>
2025-03-21 20:00:37 -0400

    update contact section

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -1,23 +1,21 @@
 function Contact() {
-  return (
-    <section id='contact' className='px-8 py-8 text-center sm:px-14'>
-      <h2 className='mb-6 text-3xl font-bold text-custom-light-tarawera dark:text-custom-dark-off-by-one sm:text-4xl md:mb-8'>
-        Get in touch
-      </h2>
-      <p className='mb-9 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray sm:px-16 md:px-36'>
-        I am currently looking for Javascript, React and Node.js opportunities (full-time
-        or freelance).
-        <br />
-        If you have any questions, think we&apos;re a fit or just want to say hi, feel
-        free to reach out to me.
-      </p>
-      <a
-        href='mailto:contact@othmanelamnabhi.com'
-        className='inline-block rounded-sm bg-custom-light-dodger-blue px-8 py-4 font-bold text-custom-dark-off-by-one duration-300 ease-out hover:scale-105 dark:bg-custom-dark-3-days-ecchymose'>
-        Say Hello 👋
-      </a>
-    </section>
-  );
+	return (
+		<section id='contact' className='px-8 py-8 text-center sm:px-14'>
+			<h2 className='mb-6 text-3xl font-bold text-custom-light-tarawera dark:text-custom-dark-off-by-one sm:text-4xl md:mb-8'>
+				Let&apos;s Build Something Together
+			</h2>
+			<p className='mb-9 text-custom-light-shuttle-gray dark:text-custom-dark-gull-gray sm:px-16 md:px-36'>
+				I specialize in TypeScript, React, and WordPress, building scalable and
+				accessible web solutions. Whether you&apos;re hiring for a project or just
+				want to chat, feel free to reach out!
+			</p>
+			<a
+				href='mailto:contact@othmanelamnabhi.com'
+				className='inline-block rounded-sm bg-custom-light-dodger-blue px-8 py-4 font-bold text-custom-dark-off-by-one duration-300 ease-out hover:scale-105 dark:bg-custom-dark-3-days-ecchymose'>
+				Say Hello 👋
+			</a>
+		</section>
+	);
 }
 
 export default Contact;
